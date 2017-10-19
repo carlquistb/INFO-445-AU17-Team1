f5f666e0cb1a2501c60427c2ee0ca356e8b51649
Brendan A Carlquist <carlquistb@gmail.com>
2017-10-18 23:06:33 -0700

    added comment to new file.

---
+++ populateLookupTables.sql
@@ -0,0 +1,15 @@
+/*
+Team 1: Parker, Jake, and Brendan
+INFO 445 AU17
+
+Description: This script will populate our lookup tables.
+
+lookup tables:
+	States
+	Cities
+	Genres
+	Formats
+	Editions
+	PromoCategories
+*/
+
